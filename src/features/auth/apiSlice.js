1d94183717697e31e69285f401b9ebd67a91b933
rajon-git <rajon.zhsust15@gmail.com>
2024-11-23 23:02:06 +0600

    update url

--- src/features/auth/apiSlice.js
+++ src/features/auth/apiSlice.js
@@ -3,8 +3,8 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 const apiSlice = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({
-    // baseUrl: 'https://householdserviceapi.onrender.com/',
-    baseUrl: 'http://127.0.0.1:8000/',
+    baseUrl: `${process.env.REACT_APP_BACKEND_URL}/`,
+    // baseUrl: 'http://127.0.0.1:8000/',
     credentials: 'include',
     prepareHeaders: (headers) => {
       const token = localStorage.getItem('token');
